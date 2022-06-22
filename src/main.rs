38333cb5066151ea45a937ff98f31b41992d41b9
Jacob Root <otis@otisroot.com>
2022-06-22 01:15:18 -0700

    Added fallback in case of death
    Signed-off-by: Jacob Root <otis@otisroot.com>

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ mod tests;
 use chess::{self, BoardStatus, ChessMove};
 use chess::{Board, Color};
 use log::debug;
+use search::transposition_table::{Flag, TransTable, TransTableEntry};
 use std::io;
 use std::str::FromStr;
-use search::transposition_table::{Flag, TransTable, TransTableEntry};
 
 fn main() {
     env_logger::init();
@@ -20,7 +20,25 @@ fn player_play() {
         let stdin = io::stdin(); // We get `Stdin` here.
         stdin.read_line(&mut buffer).unwrap();
         let player_move = ChessMove::from_str(buffer.trim()).unwrap();
-        board = board.make_move_new(player_move);
+        if board.legal(player_move) {
+            board = board.make_move_new(player_move);
+        } else {
+            debug!("Enter an FEN of the position AFTER you moved: ");
+            let mut buffer = String::new();
+            stdin.read_line(&mut buffer).unwrap();
+            match Board::from_str(buffer.trim()) {
+                Ok(board_new) => {
+                    board = board_new;
+                    debug!("Successfully made new board");
+                }
+                Err(_) => {
+                    debug!("{}", buffer);
+
+                    debug!("FAILED at parsing FEN");
+                    board = Board::default();
+                }
+            }
+        }
 
         if board.status() == BoardStatus::Checkmate || board.status() == BoardStatus::Stalemate {
             break;
@@ -28,7 +46,8 @@ fn player_play() {
 
         let color_to_move = Color::Black;
 
-        let engine_move = search::iterative_deepening_search(board, color_to_move, 7, &mut transposition_table);
+        let engine_move =
+            search::iterative_deepening_search(board, color_to_move, 5, &mut transposition_table);
         board = board.make_move_new(engine_move);
         println!("Engine move: {}", engine_move);
 
@@ -43,7 +62,8 @@ fn testing() {
     let board =
         Board::from_str("r1b1k2r/1pQp1ppp/p1n1p3/2b5/2p1PN1q/2N2B2/PPP2P1P/2KR3R b kq - 0 1")
             .expect("Invalid FEN");
-    let best_move = search::iterative_deepening_search(board, color_to_move, 7, &mut TransTable::new());
+    let best_move =
+        search::iterative_deepening_search(board, color_to_move, 7, &mut TransTable::new());
     debug!("Test");
 
     println!("Top Engine Move: {}", best_move);
